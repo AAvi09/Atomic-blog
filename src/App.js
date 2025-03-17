385d2abbd38d53e2c3ffaa6484d3c6305f99007b
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-17 23:33:15 +0530

    Optimize Archive component by memoizing to prevent unnecessary re-renders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, memo } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -164,7 +164,7 @@ function List() {
   );
 }
 
-function Archive() {
+const Archive = memo(function Archive() {
   const { onAddPost } = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
@@ -195,7 +195,7 @@ function Archive() {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
